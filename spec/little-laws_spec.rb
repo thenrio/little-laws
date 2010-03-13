0cdff9eda22ce367ae6b45e1311faba7369a4f5c
thierry <thierry.henrio@gmail.com>
2010-03-13 15:35:10 +0100

    concrete mean

--- spec/little-laws_spec.rb
+++ spec/little-laws_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "mean_cycle_time" do
-  it 'should return 0 for empty times' do
+  it 'should return 0 for empty values' do
     mean_cycle_time().should == 0
+  end
+
+  it 'should return 2.8 for *[3, 5, 3, 1, 2]' do
+    mean_cycle_time(3, 5, 3, 1, 2).should == 2.8 
   end
 end
