607743d170662fa57fe02fe0017a392ad53e5266
thierry <thierry.henrio@gmail.com>
2010-03-13 15:45:41 +0100

    rename method

--- lib/little-laws.rb
+++ lib/little-laws.rb
@@ -1,4 +1,4 @@
-def mean_cycle_time(*values)
+def mean(*values)
   return 0 if values.empty?
   sum = 0
   values.inject {|sum, n| sum+n} / Float(values.length)
--- spec/little-laws_spec.rb
+++ spec/little-laws_spec.rb
@@ -2,10 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "mean_cycle_time" do
   it 'should return 0 for empty values' do
-    mean_cycle_time().should == 0
+    mean().should == 0
   end
 
   it 'should return 2.8 for *[3, 5, 3, 1, 2]' do
-    mean_cycle_time(3, 5, 3, 1, 2).should == 2.8 
+    mean(3, 5, 3, 1, 2).should == 2.8
   end
 end
